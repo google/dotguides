907ee74b3f672f58588ef83d2fa4b9a4c08b788e
Michael Bleigh <bleigh@google.com>
2025-08-28 11:16:16 -0700

    fix rules command

--- src/cli/commands/rules.ts
+++ src/cli/commands/rules.ts
@@ -2,5 +2,5 @@ import { Workspace } from "../../lib/workspace.js";
 
 export async function rulesCommand() {
   const workspace = await Workspace.load([process.cwd()]);
-  console.log(workspace.systemInstructions());
+  console.log(await workspace.systemInstructions());
 }
